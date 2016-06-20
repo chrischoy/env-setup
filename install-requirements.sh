2dbdd9efe31ffce31b3e3a488b475f700f95b3e9
Christopher B. Choy <chrischoy@ai.stanford.edu>
2016-06-20 11:47:14 -0700

    initial commit

---
+++ install-requirements.sh
@@ -0,0 +1,11 @@
+sudo apt-get install software-properties-common
+sudo add-apt-repository ppa:neovim-ppa/unstable
+sudo apt-get update
+sudo apt-get install neovim
+udo apt-get install python-dev python-pip python3-dev python3-pip
+sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
+sudo update-alternatives --config vi
+sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
+sudo update-alternatives --config vim
+sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
+sudo update-alternatives --config editor
