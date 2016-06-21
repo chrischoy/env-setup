233a4f456190e9d32ea9773d5caa0a4069db292b
Christopher B. Choy <chrischoy@ai.stanford.edu>
2016-06-21 15:44:25 -0700

    zsh + nvim + tmux

--- install-requirements.sh
+++ install-requirements.sh
@@ -1,11 +1,15 @@
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:neovim-ppa/unstable
+# Common installations
+sudo apt-get install -y python-software-properties software-properties-common
+sudo apt-get install python-dev python-pip python3-dev python3-pip
+sudo apt-get install exuberant-ctags zsh tig
+
+# Add PPAs
+sudo add-apt-repository ppa:webupd8team/y-ppa-manager  # ppa-manager
+sudo add-apt-repository ppa:neovim-ppa/unstable  # Neovim
+sudo add-apt-repository ppa:byobu/ppa # tmux
 sudo apt-get update
+
+# Install neovim, tmux, GUI ppa-manager
+sudo apt-get install y-ppa-manager
 sudo apt-get install neovim
-udo apt-get install python-dev python-pip python3-dev python3-pip
-sudo update-alternatives --install /usr/bin/vi vi /usr/bin/nvim 60
-sudo update-alternatives --config vi
-sudo update-alternatives --install /usr/bin/vim vim /usr/bin/nvim 60
-sudo update-alternatives --config vim
-sudo update-alternatives --install /usr/bin/editor editor /usr/bin/nvim 60
-sudo update-alternatives --config editor
+sudo apt-get install tmux=2.1-3build1~trusty
---
+++ setting.sh
@@ -0,0 +1,30 @@
+# Generate a ssh key.
+ssh-keygen -t rsa -b 4096 -C "chrischoy@ai.stanford.edu"
+
+# Set up my profile
+git config --global user.email "chrischoy@ai.stanford.edu"
+git config --global user.name "Christopher B. Choy"
+
+# Neovim vim-plug install
+curl -fLo ~/.config/nvim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# TPM
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
+# Link vimrc
+ln config/.nvimrc ~/.config/nvim/init.vim
+ln config/.nvimrc ~/.nvimrc
+ln config/.config/.tmux.conf ~/.tmux.conf
+
+# Oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+# Download my custom theme
+mkdir $ZSH_CUSTOM/themes
+curl -fsSL -o $ZSH_CUSTOM/themes/capri7.zsh-theme https://gist.githubusercontent.com/chrischoy/a1a4f3e9468efd61a4ff4e3f67c945c5/raw/capri7.zsh-theme
+
+# Powerline fonts
+# Once installed, manually set the terminal font to be one of the powerline fonts
+git clone https://github.com/powerline/fonts
+sh fonts/install.sh
